e86834f90e244487371eab134d7352cb30006a73
kyuhyunp <kyuhyunp@umich.edu>
2023-01-23 21:43:18 -0800

    Set up React with four hard-coded buttons. Run npm start in my-app to test

---
+++ my-app/src/display.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+class Display extends React.Component {
+    render() {
+      return (
+        
+        <div >
+          WhatToEat
+          <br />
+          <br />
+          <br />
+            <button>
+                Chinese
+            </button>
+            <button>
+                American
+            </button>
+            <button>
+                Mexican
+            </button>
+            <button>
+                Korean
+            </button>
+        </div>
+      );
+    }
+  }
+
+export default Display;
